5756e64f2bf47470d675cda2f6a435f73acf007b
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-03-11 19:25:36 +0100

    V8

--- src/main/java/com/visionrent/dto/ContactMessageDTO.java
+++ src/main/java/com/visionrent/dto/ContactMessageDTO.java
@@ -1,5 +1,15 @@
 package com.visionrent.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactMessageDTO {
 
     private Long id;
