d54d1929e3fdeccf1b5b8607a99d0db6fb3faf23
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-03-11 17:20:54 +0100

    V2

--- src/main/java/com/visionrent/controller/ContactMessageController.java
+++ src/main/java/com/visionrent/controller/ContactMessageController.java
@@ -3,6 +3,7 @@ package com.visionrent.controller;
 
 import com.visionrent.domain.ContactMessage;
 import com.visionrent.dto.request.ContactMessageRequest;
+import com.visionrent.mapper.ContactMessageMapper;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +17,13 @@ import javax.validation.Valid;
 @RequestMapping("/contactmessage")
 public class ContactMessageController {
 
+    private ContactMessageMapper contactMessageMapper;
+
     @PostMapping("/visitors")
     public ResponseEntity<T> createMessage(@Valid @RequestBody ContactMessageRequest contactMessageRequest){
+
+        ContactMessage contactMessage = contactMessageMapper.contactMessageRequestToContactMessage(contactMessageRequest);
+
 
         return null;
     }
--- src/main/java/com/visionrent/mapper/ContactMessageMapper.java
+++ src/main/java/com/visionrent/mapper/ContactMessageMapper.java
@@ -1,11 +1,16 @@
 package com.visionrent.mapper;
 
 
+import com.visionrent.domain.ContactMessage;
+import com.visionrent.dto.request.ContactMessageRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "sp")
+@Mapper(componentModel = "spring")
 public interface ContactMessageMapper {
 
 
+    @Mapping(target = "id",ignore = true)
+    ContactMessage contactMessageRequestToContactMessage(ContactMessageRequest contactMessageRequest);
 
 }
